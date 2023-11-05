7809fa7f69b56731d2340a98a716620210b9f3e9
indraniel <indraniel@gmail.com>
2023-11-04 23:37:51 -0500

    + refactor cli.py module (multi-file approach)
      - this file is starting to get unwieldy and large as more subcommands
        and sub-subcommands are being introduced.
    
        This refactor separates out each subcommand into its own module, and
        the main cli.py just imports the details into the main cli
        entrypoint.
    
        Hopefully this makes things easier to organize and read in the long
        run.

--- src/valve/cli.py
+++ src/valve/cli.py
@@ -4,9 +4,7 @@ import click
 from dotenv import load_dotenv
 
 from valve.version import __version__
-import valve.core.api as api
-import valve.core.auth
-import valve.utils.printer as pp
+import valve.subcommands.users as u
 
 # Load environment variables from .env file
 load_dotenv()
@@ -21,22 +19,4 @@ def cli():
     # http://newbebweb.blogspot.com/2012/02/python-head-ioerror-errno-32-broken.html
     signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 
-@cli.group("users")
-def users():
-    pass
-
-@users.command("list", short_help="show user list")
-@click.option('--debug', '-d', is_flag=True, show_default=True, default=False,
-              help="Print extra debugging output")
-@click.option('--format', '-f', default="json", required=False, type=click.Choice(pp.valid_formats),
-              help="output format")
-def list_users(debug, format):
-    """
-    Show users list
-    """
-    credentials = valve.core.auth.login(debug=debug)
-    client = api.API(debug=debug, credentials=credentials)
-    users = client.users.list()
-    printer = pp.Printer(users)
-    printer.render(format=format)
-#    printer.render(format=format, columns=['firstName', 'lastName'])
+cli.add_command(u.users)
---
+++ src/valve/subcommands/users.py
@@ -0,0 +1,26 @@
+import click
+
+import valve.core.api as api
+import valve.core.auth
+import valve.utils.printer as pp
+
+@click.group("users")
+def users():
+    pass
+
+@users.command("list", short_help="show user list")
+@click.option('--debug', '-d', is_flag=True, show_default=True, default=False,
+              help="Print extra debugging output")
+@click.option('--format', '-f', default="json", required=False, type=click.Choice(pp.valid_formats),
+              help="output format")
+def list_users(debug, format):
+    """
+    Show users list
+    """
+    credentials = valve.core.auth.login(debug=debug)
+    client = api.API(debug=debug, credentials=credentials)
+    import pdb; pdb.set_trace()
+    users = client.users.list()
+    printer = pp.Printer(users)
+    printer.render(format=format)
+#    printer.render(format=format, columns=['firstName', 'lastName'])
